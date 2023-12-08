7737e6bc709613e2beafd333631e5479d324d783
YuliyaNemchaninova <nem4ninova@mail.ru>
2023-12-08 15:24:18 +0300

    v0.1

---
+++ src/test/java/OTUS/homework/OtusHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package OTUS.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
